c4622998361ee4f0fbc4cf722550deca8aa74752
Slava Shyharenko <shyharenkoslava@gmail.com>
2025-03-06 17:35:13 +0200

    fxd

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === "serve" ? "global" : "_global"]: {},
     },
-    base: "/wip-Glance/",
+    base: "/Glance/",
     root: "src",
     build: {
       sourcemap: true,
